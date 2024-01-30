fd4a5addb1b84995c8657c4b9f95295303e3c860
matoygwapo <101642501+matoygwapo@users.noreply.github.com>
2024-01-30 19:15:00 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { ref } from 'vue'
+
+const message = ref("My first VUe app")
+const todos = ref([])
+const task = ref("")
+
+const addTask = (e)=>{
+  e.preventDefault();
+  const data = {
+        task:task.value,
+        status:false
+  }
+  todos.value.push(data)
+  task.value=""
+  console.log(todos.value)
+}
+
+</script>
+
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <form class="form" @submit="addTask">
+      <input type="text" v-model="task" placeholder="add task..." required>
+      <button type="submit">Add</button>
+    </form>
+  </div>
+</template>
+
+<style>
+.form{
+  border:1px solid;
+  padding:10px;
+  width: 200px;
+  display: flex;
+  justify-content: center;
+}
+table,tr,td,th{
+  border:1px solid;
+}
+.done{
+  background-color: rgb(160, 220, 160);
+}
+</style>
